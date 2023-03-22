62d60b786038e6db1e34fd602affed20733aed34
Pedro-Arantes <developer9pedro@gmail.com>
2023-03-22 11:59:07 -0300

    Feat: index.js style

---
+++ src/components/styled/StyledMain.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    height: 100%;
+    width: 100%;
+
+`
